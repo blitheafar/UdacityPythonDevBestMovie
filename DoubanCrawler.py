9b726fb7fe0bd0db21a10ba201c2ae7d97ea5fd2
blitheanon <blitheanon@qq.com>
2019-01-23 15:34:32 +0800

    feature: 完成任务5: 构造电影信息数据表，保存电影信息到movies.csv

--- DoubanCrawler.py
+++ DoubanCrawler.py
@@ -21,6 +21,8 @@ location_list = ['中国大陆', '美国', '香港', '台湾', '日本', '韩国
 # URL list
 url_list = []
 # 实现函数构造对应类型和地区的URL地址
+
+
 def getMovieUrl(category, location):
     url = None
     url_front = "https://movie.douban.com/tag/#/?sort=S&range=9,10&tags=电影"
@@ -39,15 +41,20 @@ with open('output.txt', 'w') as f:
             url_list.append(movie_url)
 
 # 任务2: 获取电影页面 HTML
+
+
 def getHtml(url):
-    html = expanddouban.getHtml(url,True)
+    html = expanddouban.getHtml(url, True)
     return html
 
 # 任务3: 定义电影类
 # 将电影类变量添加进list
+
+
 class Movie(object):
     """电影类"""
     # 构造函数
+
     def __init__(self, name, rate, location, category, info_link, cover_link):
         # super(Movie, self).__init__()
         self.name = name
@@ -59,7 +66,8 @@ class Movie(object):
 
     # 显示类变量
     def displayMovie(self):
-           print self.name
+        print self.name
+
 
 name = "肖申克的救赎"
 rate = 9.6
@@ -78,12 +86,12 @@ m = Movie(name, rate, location, category, info_link, cover_link)
 """
 return a list of Movie objects with the given category and location.
 """
-movies_list=[]
+movies_list = []
 def getMovies(category, location):
     # 取得电影url
-    movie_url=getMovieUrl(category,location)
+    movie_url = getMovieUrl(category, location)
     # 取得url对应电影页html
-    page_html=getHtml(movie_url)
+    page_html = getHtml(movie_url)
     # 取得html内所有电影的name,rate,info_link,cover_link
     soup = BeautifulSoup(page_html, 'html.parser')
     content_div = soup.find(id="app").find(class_="list-wp")
@@ -91,38 +99,40 @@ def getMovies(category, location):
     # 遍历获取直接子元素
     for ele in content_div.find_all("a", recursive=False):
         # 一个电影对应一个字典对象，初始化字典对象
-        movie_dic={}
-        info_link=ele.get("href")
+        movie_dic = {}
+        info_link = ele.get("href")
 
         if ele.find("p", recursive=False):
-            ele_p=ele.find("p", recursive=False)
-            name=ele_p.find("span",class_="title").get_text()
-            rate=ele_p.find("span",class_="rate").get_text()
+            ele_p = ele.find("p", recursive=False)
+            name = ele_p.find("span", class_="title").get_text()
+            rate = ele_p.find("span", class_="rate").get_text()
 
         if ele.find(class_="cover-wp"):
-            ele_cover=ele.find(class_="cover-wp")
-            cover_link=ele_cover.find(class_="pic").find("img").get("src")
+            ele_cover = ele.find(class_="cover-wp")
+            cover_link = ele_cover.find(class_="pic").find("img").get("src")
 
-        # print(name+rate+location+category+info_link+cover_link)
-        movie_dic["name"]=name
-        movie_dic["rate"]=rate
-        movie_dic["location"]=location
-        movie_dic["category"]=category
-        movie_dic["info_link"]=info_link
-        movie_dic["cover_link"]=cover_link
-
+        movie_dic["name"] = name
+        movie_dic["rate"] = rate
+        movie_dic["location"] = location
+        movie_dic["category"] = category
+        movie_dic["info_link"] = info_link
+        movie_dic["cover_link"] = cover_link
         movies_list.append(movie_dic)
 
-	# return [name,rate,location,category,info_link,cover_link]
-    # print(name+rate+location+category+info_link+cover_link)
+# 生成一组list包含电影对象
+# 任务5: 构造电影信息数据表,
+"""
+从网页上选取你最爱的三个电影类型，然后获取每个地区的电影信息后，
+我们可以获得一个包含三个类型、所有地区，评分超过9分的完整电影对象的列表。
+将列表输出到文件 movies.csv
+"""
+# 最喜欢的3类电影对象保存在movies_list中
+favorite_category_list=["剧情","喜剧","动画"]
+for category_item in favorite_category_list:
+    for location_item in location_list:
+        getMovies(category_item,location_item)
 
-getMovies(category_list[0],location_list[0])
-# print(movies_list)
-# 保存电影list到movies.csv
-# with open('movies.csv', 'w') as f:
-#     f.write(str(movies_list))
-
-# 保存电影字典到movies.csv
+# 遍历movies_list，保存电影字典到movies.csv
 with open('movies.csv', 'w') as f:
     for list_item in movies_list:
-        f.write(json.dumps(list_item, encoding="UTF-8", ensure_ascii=False)+'\n')
+        f.write(json.dumps(list_item, encoding="UTF-8", ensure_ascii=False) + '\n')
